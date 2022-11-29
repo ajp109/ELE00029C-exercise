1fb58e6a33e8102de7025f76f8976085fb373cc7
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-11-29 12:10:18 +0000

    Fix whitespace

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include "mbed.h"
 
-// Initialise D2 as an OPEN DRAIN output
-// It can be used in the same way as a normal DigitalOut
-DigitalInOut sig(D2, PIN_OUTPUT, OpenDrain, 0);
- 
-int main()
+// Initialise D2 as an OPEN DRAIN output
+// It can be used in the same way as a normal DigitalOut
+DigitalInOut sig(D2, PIN_OUTPUT, OpenDrain, 0);
+
+int main()
 {
-    while(true) {
-        for (int pulseWidth = 1000; pulseWidth < 2000; pulseWidth += 10) {
-            // Send servo pulse
-            sig = true;
-            wait_us(pulseWidth);
-            sig = false;
-            
-            // Wait for approx. 20ms
-            thread_sleep_for(20);
-        }
-    }
+    while(true) {
+        for (int pulseWidth = 1000; pulseWidth < 2000; pulseWidth += 10) {
+            // Send servo pulse
+            sig = true;
+            wait_us(pulseWidth);
+            sig = false;
+            
+            // Wait for approx. 20ms
+            thread_sleep_for(20);
+        }
+    }
 }
