256483cb917891c6aed21554256a72eef78ec220
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-11-29 09:05:35 +0000

    Commit base main.cpp for exercise

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include "mbed.h"
 
-int main() {
-    while (true) {
-        sleep();
-    }
+// Initialise D2 as an OPEN DRAIN output
+// It can be used in the same way as a normal DigitalOut
+DigitalInOut sig(D2, PIN_OUTPUT, OpenDrain, 0);
+ 
+int main()
+{
+    while(true) {
+        for (int pulseWidth = 1000; pulseWidth < 2000; pulseWidth += 10) {
+            // Send servo pulse
+            sig = true;
+            wait_us(pulseWidth);
+            sig = false;
+            
+            // Wait for approx. 20ms
+            thread_sleep_for(20);
+        }
+    }
 }
